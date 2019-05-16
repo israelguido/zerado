4bd8fc3f4b5b4fa948e25124f9af25b2cafe4c34
Ïsrael Guido <israelguido@gmail.com>
2019-05-16 10:20:02 -0300

    Alterações do modulo de pagamento braspag

--- app/code/Appstronaut/Braspag/Model/Braspag.php
+++ app/code/Appstronaut/Braspag/Model/Braspag.php
@@ -17,6 +17,7 @@ class Braspag extends \Magento\Payment\Model\Method\Cc
     protected $_canAuthorize = true;
     protected $_canCapture = true;
 
+
     /**
      * Capture Payment.
      *
@@ -128,6 +129,79 @@ class Braspag extends \Magento\Payment\Model\Method\Cc
      */
     public function makeCaptureRequest($request)
     {
+        //montar o objeto
+        $params = "{  
+   \"MerchantOrderId\":\"2017051002\",
+   \"Customer\":{  
+      \"Name\":\"Israel Guido\",
+      \"Identity\":\"35564894812\",
+      \"IdentityType\":\"CPF\",
+      \"Email\":\"comprador@braspag.com.br\",
+      \"Birthdate\":\"1991-01-02\",
+      \"Address\":{  
+         \"Street\":\"Alameda Xingu\",
+         \"Number\":\"512\",
+         \"Complement\":\"27 andar\",
+         \"ZipCode\":\"12345987\",
+         \"City\":\"São Paulo\",
+         \"State\":\"SP\",
+         \"Country\":\"BRA\",
+         \"District\":\"Alphaville\"
+      },
+      \"DeliveryAddress\":{  
+         \"Street\":\"Alameda Xingu\",
+         \"Number\":\"512\",
+         \"Complement\":\"27 andar\",
+         \"ZipCode\":\"12345987\",
+         \"City\":\"São Paulo\",
+         \"State\":\"SP\",
+         \"Country\":\"BRA\",
+         \"District\":\"Alphaville\"
+      }
+   },
+   \"Payment\":{  
+      \"Provider\":\"Simulado\",
+      \"Type\":\"CreditCard\",
+      \"Amount\":10000,
+      \"Currency\":\"BRL\",
+      \"Country\":\"BRA\",
+      \"Installments\":1,
+      \"Interest\":\"ByMerchant\",
+      \"Capture\":true,
+      \"Authenticate\":false,
+      \"Recurrent\":false,
+      \"SoftDescriptor\":\"Mensagem\",
+      \"DoSplit\":false,
+      \"CreditCard\":{  
+         \"CardNumber\":\"4551870000000181\",
+         \"Holder\":\"Nome do Portador\",
+         \"ExpirationDate\":\"12/2021\",
+         \"SecurityCode\":\"123\",
+         \"Brand\":\"Visa\",
+         \"SaveCard\":\"false\",
+         \"Alias\":\"\"
+      },
+      \"Credentials\":{  
+         \"code\":\"9999999\",
+         \"key\":\"D8888888\",
+         \"password\":\"LOJA9999999\",
+         \"username\":\"#Braspag2018@NOMEDALOJA#\",
+         \"signature\":\"001\"
+      },
+      \"ExtraDataCollection\":[  
+         {  
+            \"Name\":\"NomeDoCampo\",
+            \"Value\":\"ValorDoCampo\"
+         }
+      ]
+   }
+}";
+        $requestData = $this->mountObject($params);
+
+
+        //fazer a chada para o braspag
+
+        //capturar a resposta do gateway
         $response = ['success']; //todo implement API call for capture request.
 
         if (!$response) {
@@ -135,5 +209,10 @@ class Braspag extends \Magento\Payment\Model\Method\Cc
         }
 
         return $response;
+    }
+
+    public function mountObject($orderData)
+    {
+        return json_decode($orderData);
     }
 }
